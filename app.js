3934cca79ce2521cef36d399b93343c222400b9f
bernitos <49335410+bernitos@users.noreply.github.com>
2023-01-06 03:37:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
 
+app.get("/", (req, res) => res.type('html').send(html));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+const html = `
  <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -132,4 +141,4 @@
         <script src="scripts/index.js"></script>
     </body>
 </html>
-
+'
